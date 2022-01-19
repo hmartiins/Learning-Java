7a0c51b62c1a7642cd64b766c14eb73074e8bf7f
hmartiins <hmartins224@gmail.com>
2022-01-18 23:04:52 -0300

    Creating initial file about classes 🚀

---
+++ src/classAndMethods/Product.java
@@ -0,0 +1,9 @@
+package classAndMethods;
+
+public class Product {
+
+	String name;
+	double price;
+	double discount;
+	
+}
---
+++ src/classAndMethods/ProductTest.java
@@ -0,0 +1,26 @@
+package classAndMethods;
+
+public class ProductTest {
+
+	public static void main(String[] args) {
+		// instantiating using type inference
+		var p1 = new Product();
+
+		p1.name = "Notebook";
+		p1.price = 3500.99;
+		p1.discount = 0.50;
+		
+		// instantiating using the reserved word of the class
+		Product p2 = new Product();
+		
+		p2.name = "Keyboard 60%";
+		p2.price = 699.99;
+		p2.discount = 0.25;
+
+		double finalPriceP1 = p1.price * (1 - p1.discount);
+		double finalPriceP2 = p2.price * (1 - p2.discount);
+
+		System.out.printf("Preço total dos produtos: R$%.2f", (finalPriceP1 + finalPriceP2));
+	}
+	
+}
